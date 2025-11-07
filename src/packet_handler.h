365888d7519dfdcd2987d8c1fdf04ce1976e3806
Kanishk Sachdev <kanishksachdev@gmail.com>
2025-11-07 01:06:38 -0500

    Remove database integration from packet handler; clean up unused handler_context_t structure.

--- src/packet_handler.h
+++ src/packet_handler.h
@@ -3,13 +3,7 @@
 
 #include <pcap.h>
 #include "sniffer.h"
-#include "database.h"
 
 void packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
-
-typedef struct {
-    sniffer_t *sniffer;
-    db_t *db;
-} handler_context_t;
 
 #endif
